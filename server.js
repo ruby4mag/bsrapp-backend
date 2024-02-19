c2ab8194850232cec39f05b1f4a642ce0fb7e454
Magin George <magingeorge@gmail.com>
2024-02-19 15:10:39 +0530

    updated mongodb Service

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ import adminRoutes from "./routes/adminRoutes.js"
 import stripe from "./utils/stripe.js"
 import amqp from 'amqplib/callback_api.js';
 
-const CONN_URL = 'amqp://localhost:5672';
 let ch = null;
-amqp.connect(CONN_URL, function (err, conn) {
+amqp.connect(process.env.CONN_URL, function (err, conn) {
   conn.createChannel(function (err, channel) {
     ch = channel;
   });
