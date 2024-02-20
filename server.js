72cda92928b3e9395476237458073e8cdc31ff75
Magin George <magingeorge@gmail.com>
2024-02-20 07:48:58 +0530

    added localhost to cors

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: ["https://react.bsrsport.org"],
+    origin: ["https://react.bsrsport.org", "http://localhost:3001"],
     //origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
     methods: "GET, POST, PATCH, DELETE, PUT",
     credentials: true,
