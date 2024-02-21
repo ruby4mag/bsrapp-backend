ea065db5225650c1dc881057c24f72d9f789fd09
Magin George <magingeorge@gmail.com>
2024-02-21 16:32:36 +0530

    changed doamin in generate token to env variable

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
     secure: process.env.NODE_ENV !== "development",
     sameSite: "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000, //30days
-    domain: '.bsrsport.org'
+    domain: process.env.COOKIE_DOMAIN
   })
 }
 
