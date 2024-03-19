7acdd2a6d0cf091a894465de5d06b77cb75e0870
Magin George <magingeorge@gmail.com>
2024-03-19 19:51:29 +0530

    added otlptrace exporter

--- instrumentation.js
+++ instrumentation.js
@@ -1,14 +1,21 @@
 /*instrumentation.ts*/
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import {
   PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
+  ConsoleMetricExporter
 } from '@opentelemetry/sdk-metrics';
 
 const sdk = new NodeSDK({
   traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    url: 'http://95.217.160.248/v1/traces',
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter(),
   }),
