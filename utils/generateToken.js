41a1c9bdb1bf15fccd8001463924d9c9a9047a3e
Magin George <magingeorge@gmail.com>
2024-06-07 20:21:26 +0530

    dsad

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,10 +7,10 @@ const generateToken = (req, res, userId) => {
 
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "lax",
+    //secure: process.env.NODE_ENV !== "development",
+    //sameSite: "lax",
     maxAge: 30 * 24 * 60 * 60 * 1000, //30days
-    domain: process.env.COOKIE_DOMAIN
+    //domain: process.env.COOKIE_DOMAIN
   })
 }
 
