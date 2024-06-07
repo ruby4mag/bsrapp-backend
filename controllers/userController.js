8e943ce0b70bd73f92c26f28dabb2444a031635a
Magin George <magingeorge@gmail.com>
2024-06-07 16:47:06 +0530

    dsa

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ import sendEmail from "../utils/sendEmail.js"
 import crypto from "crypto"
 
 const loginUser = asyncHandler(async (req, res) => {
-  console.log("Request sip is " + req.ip)
+  console.log("Request ip is " + req.hostname)
   const { email, password } = req.body
 
   const user = await User.findOne({ email })
