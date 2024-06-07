a843d238bb3477cfb50ea148cec5f599093df31a
Magin George <magingeorge@gmail.com>
2024-06-07 17:26:03 +0530

    dsa

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -20,7 +20,7 @@ const generateToken = (req, res, userId) => {
       secure: process.env.NODE_ENV !== "development",
       sameSite: "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000, //30days
-    })dsa
+    })
   }
 
 
